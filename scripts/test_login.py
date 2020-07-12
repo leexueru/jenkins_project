51cfad1b2a02655133319c6b77b34d4b69c59c97
leexueru <shinestar13@163.com>
2020-07-12 15:04:59 +0800

    firts commit

---
+++ scripts/test_login.py
@@ -0,0 +1,15 @@
+class TestLogin:
+    def test(self):
+        assert 1
+
+    def test2(self):
+        assert 1
+
+    def test3(self):
+        assert 0
+
+    def test4(self):
+        assert 1
+
+    def test5(self):
+        assert 1
